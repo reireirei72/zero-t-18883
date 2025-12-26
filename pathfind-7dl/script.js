118a0c82a44772695045a21c72994fd17fe464e3
Rei <59236578+reireirei72@users.noreply.github.com>
2025-12-26 18:36:01 +0300

    Update script.js

--- pathfind-7dl/script.js
+++ pathfind-7dl/script.js
@@ -69,8 +69,11 @@ $(document).ready(function(){
                 tableHtml += "</tr>";
             }
 
-            tableHtml += `</table><div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" data-id="${i}" data-lab="${labFullName}" class="bi bi-arrow-up-square-fill locCopy" viewBox="0 0 16 16"><rect x="2" y="2" width="12" height="12" rx="2" class="arrow-bg"/><path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0"/></svg>
-<i>${nameShort} - ${i}</i></div></div>`;
+            tableHtml += `</table><div>`;
+            if (i !== loc) {
+                tableHtml += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" data-id="${i}" data-lab="${labFullName}" class="bi bi-arrow-up-square-fill locCopy" viewBox="0 0 16 16"><rect x="2" y="2" width="12" height="12" rx="2" class="arrow-bg"/><path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0"/></svg>`;
+            }
+            tableHtml += ` <i>${nameShort} - ${i}</i></div></div>`;
         }
         $("#res").html(tableHtml);
     });
