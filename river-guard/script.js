25ad9e8a6cf9da97ffe0e93bd964de2fa9ddec5d
Rei <59236578+reireirei72@users.noreply.github.com>
2026-02-10 11:04:14 +0300

    Update script.js
    смена баллов

--- river-guard/script.js
+++ river-guard/script.js
@@ -446,7 +446,7 @@ $(document).ready(function() {
 			// if (!nar) {
 			// 	count_out[doz.cat].doz++;
 			// }
-			let points = Math.round(doz.diff / 30 * 0.5 * 100) / 100;
+			let points = Math.floor(doz.diff);//Math.round(doz.diff / 30 * 0.5 * 100) / 100;
 			count_out[doz.cat].doz += points;
 			if (count.medals.mzauvzhp[doz.cat]) {
 				const mzcat = count.medals.mzauvzhp[doz.cat];
